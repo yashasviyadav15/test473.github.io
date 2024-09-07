6a349801f1f815c365d6db69339c08fc40598aff
Yashasvi Yadav <46294011+yashasviyadav15@users.noreply.github.com>
2024-09-07 12:52:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginPage from './loginPage'
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
